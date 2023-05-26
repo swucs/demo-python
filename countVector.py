6bd0c47cea06db2184ba1cc993f556fe2ef74c9f
hannah.park <hannah.park@obigo.com>
2023-05-26 13:43:37 +0900

    commit

--- countVector.py
+++ countVector.py
@@ -1,9 +1,35 @@
+# https://dacon.io/en/competitions/official/235946/codeshare/6017
+
+import os
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-count_vect = CountVectorizer()
+# Get the list of txt files in the directory
 
-lines = ["나는 밥을 먹는다", "데이터레이크는 무엇인가", "나는 사과냐?", "단어의 뜻이 무엇이냐"]
-fect_vect = count_vect.fit_transform(lines)
+path_dir = "D:/OneDrive - Obigo Inc/문서/LDA"
+txt_files = os.listdir(path_dir)
 
-feature_names = count_vect.get_feature_names_out()
-print(feature_names)
+
+# 파일의 전체경로
+txt_file_name = 'US10073191B2.txt'
+file_fullPath = path_dir + '/' + txt_file_name
+
+
+# 파일 내용 읽기
+with open(file_fullPath, "rt", encoding="UTF-8") as f:
+    file_lines = f.readlines()
+    txt = " ".join(file_lines)  #파일내용
+    count_vect_file = CountVectorizer(stop_words='english', ngram_range=(1,2))
+    dict_word_counts = {}
+    if file_lines:
+        # 문장을 토큰으로 잘라서 단어목록으로 구성
+        word_counts = count_vect_file.fit_transform([txt]).toarray()[0] #2차원배열 반환함
+        word_names = count_vect_file.get_feature_names_out()
+
+        for i, word in enumerate(word_names):
+            dict_word_counts[word] = word_counts[i]
+
+        print(txt_file_name)
+        print(dict_word_counts)
+
+
--- lda.py
+++ lda.py
@@ -2,8 +2,7 @@
 
 import os
 import re
-import nltk
-from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
@@ -16,10 +15,10 @@ p_not_summary_section = re.compile('\【[A-Za-z\s]+\】')              #[xxx]섹
 
 # Merge all the txt files into one text
 total_lines = []
-#문서별 단어List
-dict_words = {}
+#문서별 단어Dictionay
+dict_file_words = {}
 #문서별 SUMMARY
-dict_summary = {}
+dict_file_summary = {}
 for txt_file_name in txt_files:
     #파일의 전체경로
     file_fullPath = path_dir + '/' + txt_file_name
@@ -28,11 +27,16 @@ for txt_file_name in txt_files:
     with open(file_fullPath, "rt", encoding="UTF-8") as f:
         file_lines = f.readlines()
         total_lines += file_lines
-        count_vect_file = CountVectorizer()
+        count_vect_file = CountVectorizer(stop_words='english', ngram_range=(1,2))
+        txt = " ".join(file_lines)  # 파일내용TEXT
         if file_lines:
-            #문장을 토큰으로 잘라서 단어목록으로 구성
-            fect_vect = count_vect_file.fit_transform(file_lines)
-            dict_words[txt_file_name] = count_vect_file.get_feature_names_out()
+            # 문장을 토큰으로 잘라서 단어목록으로 구성
+            word_counts = count_vect_file.fit_transform([txt]).toarray()[0]  # 2차원배열 반환함
+            word_names = count_vect_file.get_feature_names_out()
+            dict_word_counts = {}   #단어별 갯수 DICT
+            for i, word in enumerate(word_names):
+                dict_word_counts[word] = word_counts[i]
+            dict_file_words[txt_file_name] = dict_word_counts
             #SUMMARY 검색
             isSummarySection = False
             summaryText = ""
@@ -49,7 +53,7 @@ for txt_file_name in txt_files:
                     summaryText += line
 
             if summaryText:
-                dict_summary[txt_file_name] = summaryText
+                dict_file_summary[txt_file_name] = summaryText
 
 
 # for words in dict_words.values():
@@ -63,7 +67,7 @@ for txt_file_name in txt_files:
 count_vect = CountVectorizer(max_df=0.95, max_features=1000, min_df=2, stop_words='english', ngram_range=(1,2))
 
 # Transform the text into a TF-IDF matrix
-fect_vect = count_vect.fit_transform(total_lines)
+word_counts = count_vect.fit_transform(total_lines)
 
 #CountVectorizer객체 내의 전체 word의 명칭을 get_feature_names()을 통해 추출
 feature_names = count_vect.get_feature_names_out()
@@ -74,9 +78,11 @@ feature_names = count_vect.get_feature_names_out()
 lda = LatentDirichletAllocation(n_components=5)
 
 # Fit the LDA model to the TF-IDF matrix
-lda.fit(fect_vect)
+lda.fit(word_counts)
 
-def display_topics(model, feature_names, no_top_words):
+#토픽별 단어
+dict_topic_words = {}
+def save_topics(model, feature_names, no_top_words):
     for topic_index, topic in enumerate(model.components_):
         print('Topic #', topic_index)
 
@@ -86,9 +92,23 @@ def display_topics(model, feature_names, no_top_words):
 
         #top_indexes대상인 인덱스별로 feature_names에 해당하는 word feature 추출 후 join concat
         feature_concat = ' '.join([feature_names[i] for i in topic_word_index])
-        print(feature_concat)
+        # print(feature_concat)
+        # print([feature_names[i] for i in topic_word_index])
+
+        dict_topic_words[topic_index] = [feature_names[i] for i in topic_word_index]
 
 
-display_topics(lda, feature_names, 10)
+save_topics(lda, feature_names, 10)
 
+words_topic0 = dict_topic_words[0]
+print(words_topic0)
 
+for filename in dict_file_words:
+    word_count_sum = 0
+    if filename in dict_file_words:
+        dict_word_counts = dict_file_words[filename]
+
+        for word in words_topic0:
+            if word in dict_word_counts:
+                word_count_sum += dict_word_counts[word]
+    print(filename + " : " + str(word_count_sum))
